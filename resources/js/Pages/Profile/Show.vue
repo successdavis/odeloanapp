41fce7323b84c29ac419950cfc78665a989fec31
Simon David <simondavid612@gmail.com>
2022-11-26 12:46:56 +0100

    Fix2 delete button

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -59,7 +59,7 @@
                                   Edit Member Information
                               </span>
                         </Link>
-                        <Link v-if="$page.props.auth.user.account_number === '1000001003'"  method="delete" as="button" :href="`/member/${user.id}/delete`" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 mt-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-800 to-red-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                        <Link v-if="$page.props.auth.user.account_number === '1000001002' || $page.props.auth.user.account_number === '1000001003'"  method="delete" as="button" :href="`/member/${user.id}/delete`" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 mt-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-800 to-red-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
 
                             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                                   Delete Member
